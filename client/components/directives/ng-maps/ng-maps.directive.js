a41d2915d24d1d21cadb9a65b4fac717c4c6be3f
Manuel Rodriguez <rodriguez.manu@gmail.com>
2016-10-14 19:32:38 -0300

    testing fusion tables and heatmap

--- client/components/directives/ng-maps/ng-maps.directive.js
+++ client/components/directives/ng-maps/ng-maps.directive.js
@@ -31,20 +31,109 @@
 
         $scope.$watch('vm.options', function(newNames, oldNames) {
             if (newNames !== undefined) {
-                if (_.has(newNames, 'heatMap')) {
-                    if (newNames.heatMap !== undefined) {
-                        NgMap.getMap().then(function(map) {
-                            map.fusionTablesLayers[0].setOptions({
-                                query: {
-                                  select: 'location',
-                                  from: newNames.heatMap
-                              },
+
+                //Fusion tables && heatMap
+                if (_.has(newNames, 'heatMap') && newNames.heatMap !== undefined) {
+                    NgMap.getMap().then(function(map) {
+                        gapi.client.load('fusiontables', 'v1', function() {
+                            console.log(newNames.heatMap);
+                            var query = 'select col1 from ' + newNames.heatMap;
+                            var request = gapi.client.fusiontables.query.sqlGet({sql: query});
+                            request.execute(function(response) {
+                                onDataFetched(response, map);
                             });
                         });
+                    });
+                }
+
+                //transit
+                if (_.has(newNames, 'transit') && newNames.transit !== undefined) {
+                    cleanMapHeatMap();
+                }
+
+                //poi
+                if (_.has(newNames, 'kml') && newNames.kml !== undefined) {
+                    cleanMapHeatMap();
+                }
+
+            }
+        });
+
+        function cleanMapHeatMap() {
+            NgMap.getMap().then(function(map) {
+                heatmap.setMap(map);
+            });
+        }
+
+        function onDataFetched(response, map) {
+            if (response.error) {
+                alert('Unable to fetch data. ' + response.error.message +
+                    ' (' + response.error.code + ')');
+            } else {
+                drawHeatmap(extractLocations(response.rows), map);
+            }
+        }
+
+        function extractLocations(rows) {
+            // Patterns for latitude/longitude in a single field, separated by a space
+            // or comma, with optional north/south/east/west orientation
+            var numberPattern = '([+-]?\\d+(?:\\.\\d*)?)';
+            var latPattern = numberPattern + '\\s*([NS])?';
+            var lngPattern = numberPattern + '\\s*([EW])?';
+            var latLngPattern = latPattern + '(?:\\s+|\\s*,\\s*)' + lngPattern;
+            var northRegexp = /N/i;
+            var eastRegexp = /E/i;
+            var parseRegexp = new RegExp(latLngPattern, 'i');
+            var locations = [];
+            for (var i = 0; i < rows.length; ++i) {
+                var row = rows[i];
+                if (row[0]) {
+                    var parts = row[0].match(parseRegexp);
+                    if (parts) {
+                        var latString = parts[1];
+                        var latOrientation = parts[2];
+                        var lngString = parts[3];
+                        var lngOrientation = parts[4];
+                        var lat = parseFloat(latString);
+                        var lng = parseFloat(lngString);
+                        if (latOrientation) {
+                            var latAdjust = northRegexp.test(latOrientation) ? 1 : -1;
+                            lat = latAdjust * Math.abs(lat);
+                        }
+                        if (lngOrientation) {
+                            var lngAdjust = eastRegexp.test(lngOrientation) ? 1 : -1;
+                            lng = lngAdjust * Math.abs(lng);
+                        }
+                        var latLng = new google.maps.LatLng(lat, lng);
+                        locations.push(latLng);
                     }
                 }
             }
-        });
+            return locations;
+        }
+
+        function drawHeatmap(locations, map) {
+            var heatmap = new google.maps.visualization.HeatmapLayer({
+                dissipating: true,
+                gradient: [
+                  'rgba(102,255,0,0)',
+                  'rgba(147,255,0,1)',
+                  'rgba(193,255,0,1)',
+                  'rgba(238,255,0,1)',
+                  'rgba(244,227,0,1)',
+                  'rgba(244,227,0,1)',
+                  'rgba(249,198,0,1)',
+                  'rgba(255,170,0,1)',
+                  'rgba(255,113,0,1)',
+                  'rgba(255,57,0,1)',
+                  'rgba(255,0,0,1)'
+                ],
+                opacity: 0.79,
+                radius: 33,
+                data: locations
+            });
+            heatmap.setMap(map);
+        }
 
         vm.styles = [{
             featureType: 'poi',
